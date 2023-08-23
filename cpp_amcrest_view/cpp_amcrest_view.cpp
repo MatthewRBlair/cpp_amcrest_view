35d9da206c00a13fe1644f43e7e3f909918006fc
MatthewRBlair <sevenstringblues@gmail.com>
2023-08-22 19:09:08 -0500

    Add project files.

---
+++ cpp_amcrest_view/cpp_amcrest_view.cpp
@@ -0,0 +1,136 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
+#include <dpp/dpp.h>
+
+#include <fstream>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+bool bot_connected = false;
+
+map<string, string> config {
+    {"discord_bot_token", ""},
+    { "username", "" },
+    { "password", "" },
+    { "ip", "" },
+    { "port", "" },
+    { "channel", "1" },
+    { "subtype", "1" },
+    { "name", "" },
+    { "motion", "false" },
+    { "people", "false" },
+    { "discord_channel", "" },
+    { "discord_server", "" },
+    { "headless", "false" },
+};
+
+
+void read_config(string fname)
+{
+    fstream config_file;
+    config_file.open(fname, ios::in);
+
+    if (config_file.is_open())
+    {
+        string line;
+        
+        while (getline(config_file, line))
+        {
+            int d = line.find("=");
+            string key = line.substr(0, d);
+            string val = line.substr(d+1, line.length());
+            if (config.find(key) != config.end())
+            {
+                config[key] = val;
+            }
+        }
+
+        config_file.close();
+    }
+}
+
+
+int main() 
+{
+    // Read config file
+    read_config("config");
+
+    ostringstream oss;
+    if (config["port"].compare("") != 0)
+    {
+        oss << "rtsp://" << config["username"] << ":" << config["password"] << "@" << config["ip"] << ":" << config["port"] << "/cam/realmonitor?channel=" << config["channel"] << "&subtype=" << config["subtype"];
+    }
+    else
+    {
+        oss << "rtsp://" << config["username"] << ":" << config["password"] << "@" << config["ip"] << "/cam/realmonitor?channel=" << config["channel"] << "&subtype=" << config["subtype"];
+    }
+    string url = oss.str();
+
+    // Initialize VideoCapture with RTSP stream URL
+    VideoCapture cap(url);
+
+    HOGDescriptor hog;
+    hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+
+    if (!cap.isOpened()) {
+        cerr << "Error opening video stream." << endl;
+        return 1;
+    }
+
+    // Create a window to display the video
+    namedWindow("RTSP Stream", cv::WINDOW_NORMAL);
+
+    dpp::cluster bot(config["discord_bot_token"]);
+
+    bot.on_log(dpp::utility::cout_logger());
+
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        bot_connected = true;
+        });
+
+    bot.start();
+
+    while (true) {
+        Mat frame;
+        cap >> frame; // Capture frame from RTSP stream
+
+        vector<cv::Rect> detected_people;
+        hog.detectMultiScale(frame, detected_people, 1.05, cv::Size(8, 8));
+
+        // Draw rectangles around detected people
+        for (const auto& rect : detected_people) {
+            rectangle(frame, rect, Scalar(0, 0, 255), 2); // Red color (BGR format)
+        }
+
+        if (frame.empty()) {
+            cerr << "End of video stream." << endl;
+            break;
+        }
+
+        imshow("RTSP Stream", frame);
+
+        if (detected_people.size() > 0 && bot_connected)
+        {
+            imwrite("person.jpg", frame);
+            dpp::message m;
+            m.set_content("Person detected!");
+            m.channel_id = config["discord_channel"];
+            m.add_file("person.jpg", dpp::utility::read_file("person.jpg"));
+            bot.message_create(m);
+        }
+
+        // Press 'q' to exit the loop
+        if (waitKey(1) == 'q') {
+            break;
+        }
+    }
+
+    cap.release();
+    cv::destroyAllWindows();
+
+    return 0;
+}
